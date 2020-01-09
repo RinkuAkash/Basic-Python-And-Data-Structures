4b87c26340b79511ebc616ff49c1c0debb9619f5
RinkuAkash <akashrinku2@outlook.com>
2020-01-09 18:05:01 +0530

    [Akash]  Add. Added program which prints first and last color of given list

---
+++ Basic/_color_list.py
@@ -0,0 +1,14 @@
+'''
+Created on 09/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+    Write a Python program to display the first and last colors from the following list. 
+color_list = ["Red","Green","White" ,"Black"]
+'''
+
+color_list=["Red","Green","White","Black"]
+
+#printing first and last color from above list
+print(color_list[0],color_list[-1])
