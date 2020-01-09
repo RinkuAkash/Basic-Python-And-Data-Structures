62a980c246dc04593b8921e6aefea4f5a8f5cd73
RinkuAkash <akashrinku2@outlook.com>
2020-01-09 18:13:36 +0530

    [Akash] Add. Program which prints documents of built-in function

---
+++ Basic/_syntax.py
@@ -0,0 +1,14 @@
+'''
+Created on 09/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s). 
+'''
+
+#user input
+function=input()
+
+#document of function
+help(function)
