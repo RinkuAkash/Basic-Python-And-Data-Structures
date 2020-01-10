cae507702b59c74346bf04864fd8bc89b931e151
RinkuAkash <akashrinku2@outlook.com>
2020-01-10 18:38:50 +0530

    Program to extract key-value pair of dictionary in variables

---
+++ Basic/40_extract_key_value_pair.py
@@ -0,0 +1,15 @@
+'''
+created on 10/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to extract single key-value pair of a dictionary in variables. 
+'''
+
+
+Dict={'First':'1','Second':'2','Third':'3'}
+
+for k,v in Dict.items():
+    print(k)
+    print(v)
