da96c68d68403c69cd719cb497dc7b48413ea7c1
RinkuAkash <akashrinku2@outlook.com>
2020-01-10 15:17:42 +0530

    program to check size of an object

---
+++ Basic/get_size_of_object.py
@@ -0,0 +1,13 @@
+'''
+created on 10/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to get the size of an object in bytes. 
+'''
+
+import sys
+
+obj=""
+print(sys.getsizeof(obj),"bytes")
