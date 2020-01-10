d4da45af971a56563fbabd66ee6dc895b42a79f4
RinkuAkash <akashrinku2@outlook.com>
2020-01-10 15:28:10 +0530

    Program to count the number of occurrence of given char in a string

---
+++ Basic/count_char.py
@@ -0,0 +1,13 @@
+'''
+created on 10/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to count the number occurrence of a specific character in a string. 
+'''
+
+String=input("Enter a string : ")
+char=input("Enter a character to count : ")
+
+print("No of occurrences : ",String.count(char))
