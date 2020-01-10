e0b31285c3ce323fccd94f2e460338b209b211a9
RinkuAkash <akashrinku2@outlook.com>
2020-01-10 12:09:42 +0530

    [Akash] Add. Program that prints list of files

---
+++ Basic/list_all_files.py
@@ -0,0 +1,12 @@
+'''
+Created on 10/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to list all files in a directory in Python. 
+'''
+
+import os
+
+print(os.listdir())
