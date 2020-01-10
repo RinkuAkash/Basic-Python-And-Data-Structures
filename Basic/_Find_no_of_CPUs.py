3037c5d9ac78064416a7a355a07e5ddf7e08bdc2
RinkuAkash <akashrinku2@outlook.com>
2020-01-10 11:52:21 +0530

    [Akash] Add. Program that prints no. of CPU's

---
+++ Basic/_Find_no_of_CPUs.py
@@ -0,0 +1,12 @@
+'''
+Created on 10/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to find out the number of CPUs using
+'''
+
+import multiprocessing
+
+print(multiprocessing.cpu_count())
