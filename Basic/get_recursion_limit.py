68f5ba44a5e66b7a9ae8a0c9cfa6f85ec414f96f
RinkuAkash <akashrinku2@outlook.com>
2020-01-10 15:21:37 +0530

    Program to get current recursion limit

---
+++ Basic/get_recursion_limit.py
@@ -0,0 +1,12 @@
+'''
+created on 10/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to get the current value of the recursion limit. 
+'''
+
+import sys
+
+print(sys.getrecursionlimit())
