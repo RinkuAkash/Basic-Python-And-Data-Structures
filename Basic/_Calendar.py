7adab34a4b5db33971fd26736e124748214ff2af
RinkuAkash <akashrinku2@outlook.com>
2020-01-10 10:10:33 +0530

    [Akash] Add. Added calendar program which shows calendar month

---
+++ Basic/_Calendar.py
@@ -0,0 +1,18 @@
+'''
+Created on 09/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+    Write a Python program to print the calendar of a given month and year.
+'''
+
+#importing calendar module
+import calendar
+
+#Taking input from user
+year=int(input("Enter year(YYYY): "))
+month=int(input("Enter month (MM): "))
+
+print(calendar.month(year,month))
+p
