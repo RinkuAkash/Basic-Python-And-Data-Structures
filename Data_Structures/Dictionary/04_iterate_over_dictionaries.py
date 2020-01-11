4aceb1913aacadab3b148e4328c7b3ce47780e5c
RinkuAkash <akashrinku2@outlook.com>
2020-01-11 11:45:05 +0530

    program to iterate over dictionary

---
+++ Data_Structures/Dictionary/04_iterate_over_dictionaries.py
@@ -0,0 +1,13 @@
+'''
+Created on 11/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to iterate over dictionaries using for loops.
+'''
+
+dictionary={'key1':'value1','key2':'value2','key3':'value3','key4':'value4','key5':'value5','key6':'value6','key7':'value7','key8':'value8','key9':'value9'}
+
+for k,v in dictionary.items():
+    print(k,v)
