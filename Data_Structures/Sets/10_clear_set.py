de7eb01887c580cf912c59b2133bb05cbc8aa40c
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 11:42:02 +0530

    Program to clear set

---
+++ Data_Structures/Sets/10_clear_set.py
@@ -0,0 +1,14 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to clear a set
+'''
+
+Set={1,2,3,4,5,6,7,8,9}
+print("Set before clearing : ",Set)
+
+Set.clear()
+print("Set after clearing : ",Set)
