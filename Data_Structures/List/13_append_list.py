a073bc733861f627a964881f26acca56f565951b
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 15:11:22 +0530

    Program to append a list to another list

---
+++ Data_Structures/List/13_append_list.py
@@ -0,0 +1,15 @@
+'''
+Created on 13/01/2020
+@author : B Akash
+'''
+'''
+Problem statement:
+Write a Python program to append a list to the second list
+'''
+
+List1=[1,2,3,4,5]
+List2=[6,7,8,9]
+
+List1.extend(List2)
+
+print("List after appended with another list : ",List1)
