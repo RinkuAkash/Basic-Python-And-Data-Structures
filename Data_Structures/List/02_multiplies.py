8a1d78e93991004dc796475c4f461d2c986d639f
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 12:06:34 +0530

    program to multiplie all items in list

---
+++ Data_Structures/List/02_multiplies.py
@@ -0,0 +1,17 @@
+'''
+Created on 13/01/2020
+@author : B Akash
+'''
+'''
+problem statement:
+Write a Python program to multiplies all the items in a list
+'''
+
+List=[1,2,3,4,5,6,7,8,9]
+
+multiple=1
+
+for item in List:
+    multiple*=item
+
+print("all items multiplication of list : ",multiple)
