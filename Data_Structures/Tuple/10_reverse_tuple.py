02b5edbbca9aea93e48d86e3bc4db24738bc1e43
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 19:12:30 +0530

    Program to reverse the tuple

---
+++ Data_Structures/Tuple/10_reverse_tuple.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to reverse a tuple
+'''
+
+Tuple=(0,1,2,3,4,5,6,7,8,9)
+
+print("Before reverse: ",Tuple)
+print("After reverse: ",Tuple[::-1])
