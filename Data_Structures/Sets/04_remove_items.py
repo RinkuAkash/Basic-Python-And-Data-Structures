f6d6d8df4ed60bb8410dcb78a7e203939267ded4
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 11:14:38 +0530

    Program to remove items from a set

---
+++ Data_Structures/Sets/04_remove_items.py
@@ -0,0 +1,18 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python Program to remove item(s) from set
+'''
+
+Set=set({1,2,3,4,5,6,7,8,9,0})
+print("Set : ",Set)
+n=int(input("Enter number of items you want to remove : "))
+
+for _ in range(n):
+    item=int(input())
+    Set.discard(item)
+
+print("Set after removing elements : ",Set)
