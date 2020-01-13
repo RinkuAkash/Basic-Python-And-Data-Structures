e4ec805f46430c9b3bb5102d965eceb0788bb416
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 11:48:23 +0530

    Program to create frozenset

---
+++ Data_Structures/Sets/11_frozensets.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to use of frozensets
+'''
+
+Set={1,2,3,4,5,6,7,8,9}
+Frozen_Set=frozenset(Set)
+
+print("Forzen Set : ",Frozen_Set)
