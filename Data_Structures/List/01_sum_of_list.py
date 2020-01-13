69f6f62ce0148f2f994e7d516b129cc31da28660
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 11:58:37 +0530

    Program to sum all items in list

---
+++ Data_Structures/List/01_sum_of_list.py
@@ -0,0 +1,12 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to sum all the items in a list
+'''
+
+Set={1,2,3,4,5,6,7,8,9}
+
+print("Sum of list : ",sum(Set))
