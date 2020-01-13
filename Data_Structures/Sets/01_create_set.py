e767871b1d87f486b86979eee361b6b3a1ef54ce
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 10:48:27 +0530

    Program to create set

---
+++ Data_Structures/Sets/01_create_set.py
@@ -0,0 +1,15 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to create a set
+'''
+
+Set=set()
+
+size=int(input("Enter the number of items you want to add to set : "))
+for _ in range(size):
+    Set.add(input())
+print("Your set is : ",Set)
