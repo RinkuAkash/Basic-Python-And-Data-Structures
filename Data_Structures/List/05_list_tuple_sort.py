49cd0b29124f779504c97e163394708ef20bd72f
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 12:31:08 +0530

    Program to sort list of tuple by last element of tuples

---
+++ Data_Structures/List/05_list_tuple_sort.py
@@ -0,0 +1,16 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.   
+	Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+	Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+'''
+
+List=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+
+def last(n):
+    return n[-1]
+print(sorted(List,key=last))
