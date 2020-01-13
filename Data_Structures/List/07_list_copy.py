4a4473dad087bbca86e62aef3f9c81b736d64050
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 12:52:46 +0530

    Program to copy list

---
+++ Data_Structures/List/07_list_copy.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to clone or copy a list
+'''
+
+List1=[1,2,3,4,5,6,7,8,9]
+List2=List1.copy()
+
+print("Copied List : ",List2)
