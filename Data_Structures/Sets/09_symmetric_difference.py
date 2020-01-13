bf99a6208b1fb6fbfaef98a4b76f6991345978a4
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 11:37:47 +0530

    Program to create symmetric difference

---
+++ Data_Structures/Sets/09_symmetric_difference.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to create a symmetric difference
+'''
+
+Set1={1,2,3,4,5,6}
+Set2={4,5,6,7,8,9}
+
+print("Symmetric difference of Set1 and Set2 : ",Set1.symmetric_difference(Set2))
