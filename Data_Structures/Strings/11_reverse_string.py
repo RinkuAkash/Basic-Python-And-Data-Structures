123cb4d386680a999117d2fc71534e3a2c7985a1
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 17:55:32 +0530

    Program to reverse the string

---
+++ Data_Structures/Strings/11_reverse_string.py
@@ -0,0 +1,11 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to reverse a string
+'''
+
+String=input("Enter a String : ")
+print(String[::-1])
