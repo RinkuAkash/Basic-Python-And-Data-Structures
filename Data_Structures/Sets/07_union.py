8fb82f36b6b25854c1993a1d228786c502179cc5
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 11:28:47 +0530

    Program to create union of sets

---
+++ Data_Structures/Sets/07_union.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to create a union of sets
+'''
+
+Set1={1,2,3,4,5,6}
+Set2={7,8,9,0}
+
+print("Union of Set1 and Set2 : ",Set1.union(Set2))
