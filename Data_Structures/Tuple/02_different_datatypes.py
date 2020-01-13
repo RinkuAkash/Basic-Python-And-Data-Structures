2813b58fdef6149e5eea166a2baa1311f589cb2b
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 18:24:04 +0530

    Program to create tuple with different datatypes

---
+++ Data_Structures/Tuple/02_different_datatypes.py
@@ -0,0 +1,14 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python Program to create a tuple with different data types
+'''
+
+Tuple=tuple((1,"String",0.125,True))
+
+print(Tuple)
+for item in Tuple:
+    print(type(item))
