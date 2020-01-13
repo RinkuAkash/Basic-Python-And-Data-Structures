284475ea9c9c97dbe564807df4dd025dc15cd093
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 17:23:38 +0530

    Program to convert input string to lower and upper case

---
+++ Data_Structures/Strings/06_lower_upper.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python script that takes input from the user and displays that input back in upper and lower cases. 
+'''
+
+String=input("Enter the string : ")
+
+print("Uppercase string : ",String.upper())
+print("Lowercase string : ",String.lower())
