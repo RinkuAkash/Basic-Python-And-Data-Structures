917bf7c957294efb94f2a07278173b7daf6bb639
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 17:40:26 +0530

    Program to get last part before specified character

---
+++ Data_Structures/Strings/08_last_part.py
@@ -0,0 +1,14 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+ Write a Python program to get the last part of a string before a specified character. 
+	https://www.w3resource.com/python-exercises
+	https://www.w3resource.com/python
+'''
+
+String=input()
+
+print(String.split('-')[0])
