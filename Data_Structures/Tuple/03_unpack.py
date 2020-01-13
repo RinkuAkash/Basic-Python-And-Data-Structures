4761070a087836688b0f72e2b9ca40cac4004881
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 18:24:38 +0530

    Program to unpack tuple values to variables

---
+++ Data_Structures/Tuple/03_unpack.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to unpack a tuple in several variables
+'''
+Tuple=tuple((1,"String",0.125,True))
+
+a,b,c,d=Tuple
+
+print("a={0}, b={1}, c={2}, d={3}".format(a,b,c,d))
