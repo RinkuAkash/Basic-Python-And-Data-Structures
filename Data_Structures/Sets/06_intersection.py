40bf53d6d979e52cbf78f055311d1d4aeaa7d863
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 11:21:52 +0530

    Program to create set intersection of sets

---
+++ Data_Structures/Sets/06_intersection.py
@@ -0,0 +1,13 @@
+'''
+created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to create an intersection of sets
+'''
+
+Set1={1,2,3,4,0}
+Set2={5,6,7,8,0}
+
+print("Intersection of set : ",Set1.intersection(Set2))
