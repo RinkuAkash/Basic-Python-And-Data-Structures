7dcc0d50587e875ce6962f646a24a0f55f49933a
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 17:04:16 +0530

    Program to calculate the length of string

---
+++ Data_Structures/Strings/01_calculate_length.py
@@ -0,0 +1,14 @@
+'''
+Created on 13/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to calculate the length of a string
+'''
+
+String=input("Enter string : ")
+count=0
+for _ in String:
+    count+=1
+print(count)
