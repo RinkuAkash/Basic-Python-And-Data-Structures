98f2d348c018cc844a5c9e4cb26f3d77e1833d63
RinkuAkash <akashrinku2@outlook.com>
2020-01-13 11:33:27 +0530

    Program to create set difference

---
+++ Data_Structures/Sets/08_set_difference.py
@@ -0,0 +1,13 @@
+'''
+Create on 13/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python program to create a set difference
+'''
+
+Set1={1,2,3,4,5,6}
+Set2={4,5,6,7,8,9}
+
+print("Set difference of Set1 and Set2 : ",Set1.difference(Set2))
