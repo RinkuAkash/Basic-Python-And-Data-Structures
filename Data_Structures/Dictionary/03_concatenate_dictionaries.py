0b164cc44d8f8902c1e47543bf1575729855f910
RinkuAkash <akashrinku2@outlook.com>
2020-01-14 15:05:37 +0530

    Added keys and values using for loop

--- Data_Structures/Dictionary/03_concatenate_dictionaries.py
+++ Data_Structures/Dictionary/03_concatenate_dictionaries.py
@@ -15,6 +15,11 @@ dic1={1:10,2:20}
 dic2={3:30,4:40}
 dic3={5:50,6:60}
 
-dic1.update(dic2)
-dic1.update(dic3)
+#Merging dic2 into dic1
+for k,v in dic2.items():
+    dic1[k]=v
+#Merging dic3 to updated dic1
+for k,v in dic3.items():
+    dic1[k]=v
+
 print(dic1)
