f249fc8d7213d24d109e4cfbdaa33cd117a5903f
RinkuAkash <akashrinku2@outlook.com>
2020-01-14 15:02:33 +0530

    Added comments

--- Data_Structures/Dictionary/02_add_key.py
+++ Data_Structures/Dictionary/02_add_key.py
@@ -9,7 +9,11 @@ Write a Python script to add a key to python
 
 Dict={0:10,1:20}
 
+#Taking user input
 key,value=map(str,input("Enter key and value (separating with space) : ").split())
 
+#Adding user key and value to dictionary
 Dict[key]=value
+
+#printing dictionary
 print(Dict)
