cda788eb4c109db27c26b92c02c98d9c190d760d
RinkuAkash <akashrinku2@outlook.com>
2020-01-14 15:09:36 +0530

    Removed sum function and calculated sum

--- Data_Structures/List/01_sum_of_list.py
+++ Data_Structures/List/01_sum_of_list.py
@@ -9,4 +9,8 @@ Write a Python program to sum all the items in a list
 
 List=[1,2,3,4,5,6,7,8,9]
 
-print("Sum of list : ",sum(List))
+sum=0
+for i in range(0,len(List)):
+    sum+=List[i]
+
+print("Sum of list : ",sum)
