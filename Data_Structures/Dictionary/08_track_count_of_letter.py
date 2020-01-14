4f548e2cb81d15195e0a89ae7468e854fd0f520b
RinkuAkash <akashrinku2@outlook.com>
2020-01-14 15:08:21 +0530

    Frequency of charactor is counted using loop

--- Data_Structures/Dictionary/08_track_count_of_letter.py
+++ Data_Structures/Dictionary/08_track_count_of_letter.py
@@ -9,12 +9,19 @@ Write a Python program to create a dictionary from a string.
 	Sample string : 'w3resource'
 	Expected output: {'3': 1, 's': 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}
 '''
-
+#Taking user input
 string=input("Enter a string : ")
 
+#initialising Dictionary
 Dict={}
 
+#looping and counting characters
 for i in range(0,len(string)):
-    Dict[string[i]]=string.count(string[i])
-
+	count=0
+	for j in range(0,len(string)):
+		if string[i]==string[j]:
+			count+=1
+	#adding character and count of frequency to dictionary
+	Dict[string[i]]=count
+#printing Dictionary
 print(Dict)
