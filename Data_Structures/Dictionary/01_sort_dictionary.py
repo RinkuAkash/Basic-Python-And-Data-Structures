1572454ebf57e1910117c84f12cce4ca5e11f02d
RinkuAkash <akashrinku2@outlook.com>
2020-01-14 15:01:31 +0530

    Wrote for loops to sort and get sorted dictionaries

--- Data_Structures/Dictionary/01_sort_dictionary.py
+++ Data_Structures/Dictionary/01_sort_dictionary.py
@@ -9,8 +9,32 @@ Write a Python script to sort (ascending and descending) a dictionary by value.
 #initializing dictionary
 Dict={1:2,3:4,8:9,5:6}
 
-#sorting dictionary 
-ascending_order={k:v for k,v in sorted(Dict.items(), key= lambda kv:kv[1] )}
-descending_order={k:v for k,v in sorted(Dict.items(), key=lambda kv: kv[1],reverse=True)}
-print("Ascending order  : ",ascending_order)
-print("Descending order : ",descending_order)
+#Getting dictionary values
+values=list(Dict.values())
+
+#sorting dictionary values
+for i in range(0,len(values)):
+    for j in range(0,len(values)-i-1):
+        if values[j]>values[j+1]:
+            values[j+1],values[j]=values[j],values[j+1]
+
+#initialising Ascending order dictionary
+Ascending_order={}
+
+#Adding keys and values to dictionary according to sorted values
+for value in values:
+    for k,v in Dict.items():
+        if v==value:
+            Ascending_order[k]=v
+
+#initialising Descending order Dictionary
+Descending_order={}
+#Adding values to Dictionary according to reverse order of values
+for value in values[::-1]:
+    for k,v in Dict.items():
+        if v==value:
+            Descending_order[k]=v
+
+#printing ordered dictionaries
+print("Ascending order  : ",Ascending_order)
+print("Descending order : ",Descending_order)
