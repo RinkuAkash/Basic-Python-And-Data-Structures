c846cc484f7307940f25227bff9fcb98131d395c
RinkuAkash <akashrinku2@outlook.com>
2020-01-14 12:11:47 +0530

    Refactored. modified code in more readable way

--- Data_Structures/Dictionary/09_dictionary_in_table.py
+++ Data_Structures/Dictionary/09_dictionary_in_table.py
@@ -7,7 +7,20 @@ Problem statement:
 Write a Python program to print a dictionary in table format.
 '''
 
-dictionary={'key':['value1','value2','value3'],'int':[1,2,3],'float':[1.0,2.0,3.0],'bool':['True','False','True']}
+dictionary={'key':['value1','value2','value3'],'int':[1,2],'float':[1.0,2.0,3.0],'bool':['True','False','True']}
 
-for row in zip(*([key]+(value) for key,value in dictionary.items())):
-    print(*row)
+for key in dictionary.keys():
+    print(key,end=" ")
+print()
+length=float('-inf')
+for value in dictionary.values():
+    if len(value)>length:
+        length=len(value)
+for i in range(0,length):
+    for key in dictionary.keys():
+        List=dictionary.get(key)
+        try:
+            print(List[i],end=" ")
+        except:
+            print(" ",end=" ")
+    print()
