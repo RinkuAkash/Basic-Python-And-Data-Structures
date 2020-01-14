5b0b9c13a7b66dacc7d47627f9b3d5a9eb23b4b0
RinkuAkash <akashrinku2@outlook.com>
2020-01-14 12:11:57 +0530

    Refactored. modified code in more readable way

--- Data_Structures/Dictionary/09_dictionary_in_table.py
+++ Data_Structures/Dictionary/09_dictionary_in_table.py
@@ -9,13 +9,22 @@ Write a Python program to print a dictionary in table format.
 
 dictionary={'key':['value1','value2','value3'],'int':[1,2],'float':[1.0,2.0,3.0],'bool':['True','False','True']}
 
+#printing keys in a row
 for key in dictionary.keys():
     print(key,end=" ")
+
+#skiping line
 print()
+
+#initilising length negative infinite
 length=float('-inf')
+
+#getting maximum length of dictionary values
 for value in dictionary.values():
     if len(value)>length:
         length=len(value)
+
+#printing each value in row wise
 for i in range(0,length):
     for key in dictionary.keys():
         List=dictionary.get(key)
